46aa9db2a098fb4d36cf333c8d6dd4f18a34315a
Jason Sakellariou <js.sakel@gmail.com>
2018-07-27 12:10:39 +0300

    update cython_setup

--- cython/cython_setup.py
+++ cython/cython_setup.py
@@ -2,8 +2,7 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 
-
 setup(
-    ext_modules=cythonize("cy_aliassampler.pyx"),
+    ext_modules=cythonize("../aliassampler_cy.pyx"),
     include_dirs=[numpy.get_include()]
 )
