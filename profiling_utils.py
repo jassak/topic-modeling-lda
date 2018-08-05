ff618e0defcdd923c73aa7f7d06cd9f0e8077944
Jason Sakellariou <js.sakel@gmail.com>
2018-08-05 18:02:39 +0300

    add profileit decorator

---
+++ profiling_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Created on 5 August 2018
+
+@author: jason
+"""
+
+import cProfile, pstats, io
+
+def profileit(fnc):
+    """A decorator that uses cProfile to profile a function"""
+
+    def inner(*args, **kwargs):
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = fnc(*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retval
+
+    return inner
